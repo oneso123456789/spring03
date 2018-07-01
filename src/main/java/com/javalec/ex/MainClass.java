a7a2436908c17458ec389ccff94fce8ba763dc42
tori1 <tori1@220.74.45.37>
2018-07-01 22:29:22 +0900

    spring 인강 3장

---
+++ src/main/java/com/javalec/ex/MainClass.java
@@ -0,0 +1,25 @@
+package com.javalec.ex;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String configLoc = "classpath:applicationCTX.xml";
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext(configLoc);
+		MyCalculator myCalculator = ctx.getBean("mycalculator",MyCalculator.class); //getBean의 첫번째 객체엔 xml에서 선언한 bean의  id가 들어가야함;
+																					//아니 근대 이거 당연한건대 내가 멍청했음;
+		
+		myCalculator.add();
+		myCalculator.sub();
+		myCalculator.mult();
+		myCalculator.div();
+		
+		
+		
+	}
+
+}
